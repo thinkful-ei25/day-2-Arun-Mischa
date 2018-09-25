e072b742dd77901ca866e74b8ae1a4cfcb4d2896
mischb <mischarberlin@gmail.com>
2018-09-25 12:36:04 -0400

    added repeat fn

--- drills-1.js
+++ drills-1.js
@@ -47,3 +47,19 @@ function average(numbers) {
 }
 
 console.log(average(numbersArray));
+
+
+function repeat(fn,n){
+  for(let i = 0; i < n; i++){
+    fn();
+  }
+}
+function hello(){
+  console.log("hello world");
+}
+function goodbye(){
+  console.log("goodbye world");
+}
+
+repeat(hello,5);
+repeat(goodbye, 5);
